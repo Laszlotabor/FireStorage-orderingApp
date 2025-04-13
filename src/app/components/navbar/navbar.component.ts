6fff7680a09e6857613147d30ccabfbe61925b78
LesButcher <laszlotabor@gmail.com>
2025-04-13 13:46:43 +0200

    7th save  save

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { RouterLink } from '@angular/router';
+import * as bootstrap from 'bootstrap';
+
 
 
 @Component({
@@ -7,8 +10,22 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
 
+  constructor(private router: Router) { 
+      this.router.events.subscribe((event) => {
+        if (event instanceof NavigationEnd) {
+          this.closeNavbar();
+        }
+      });
+  };
+  
+  closeNavbar() {
+    const navbarToggle = document.querySelector('.navbar-collapse');
+    if (navbarToggle?.classList.contains('show')) {
+      new bootstrap.Collapse(navbarToggle).toggle();
+    }
+  }
 }
